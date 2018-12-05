3cab16650a089024b13466d7da77e889bc3c683f
brnls <brnls1234@gmail.com>
2018-12-04 20:52:54 -0800

    Day2 pt 2

--- Day2.fs
+++ Day2.fs
@@ -1,6 +1,45 @@
 ﻿module Day2
 
 open System.IO
-open System
 
-let data = File.ReadAllLines("day2input.txt") |> Array.map Int32.Parse
+let data = File.ReadAllLines("day2input.txt")
+
+let part1() = 
+    let counts (word: string) =
+        let letterCount = Array.zeroCreate 26 
+        for c in word do
+            letterCount.[int c - 97] <- letterCount.[int c - 97] + 1
+        let mutable exactlyTwo = false
+        let mutable exactlyThree = false
+        let mutable i = 0
+        while i < letterCount.Length && (not exactlyTwo || not exactlyThree) do
+            if letterCount.[i] = 2 then exactlyTwo <- true
+            elif letterCount.[i] = 3 then exactlyThree <- true
+            i <- i + 1
+        exactlyTwo, exactlyThree
+
+    data 
+    |> Array.map counts
+    |> Array.fold (fun (sx, sy) (x,y) ->  (if x then sx + 1 else sx), (if y then sy + 1 else sy)) (0, 0)
+    |> (fun (x,y) -> x * y)
+
+let part2() =
+    let compare (id1:string, id2:string) =
+        let rec inner idx diffIdx =
+            if idx = id1.Length then 
+                diffIdx |> Option.map (fun i -> id1.Remove(i, 1))
+            else
+            match diffIdx with
+            |None ->
+                if id1.[idx] <> id2.[idx] then
+                    inner (idx + 1) (Some idx)
+                else inner (idx + 1) None
+            |Some _ ->
+                if id1.[idx] <> id2.[idx] then
+                    None
+                else inner (idx + 1) diffIdx
+        inner 0 None
+
+    Seq.allPairs data data 
+    |> Seq.choose compare
+    |> Seq.head 
--- Script1.fsx
+++ Script1.fsx
@@ -3,15 +3,3 @@
 Directory.SetCurrentDirectory(__SOURCE_DIRECTORY__)
 
 let data = File.ReadAllLines("day2input.txt")
-
-let counts (word: string) =
-    let letterCount = Array.zeroCreate 26 
-    for c in word do
-        letterCount.[int c - 97] <- letterCount.[int c - 97] + 1
-    letterCount |> Array.exists(fun x -> x = 2),
-    letterCount |> Array.exists(fun x -> x = 3)
-
-let getChecksum() =
-    let counts = data |> Array.map counts
-    (counts |> Array.filter fst).Length * (counts |> Array.filter snd).Length
-
